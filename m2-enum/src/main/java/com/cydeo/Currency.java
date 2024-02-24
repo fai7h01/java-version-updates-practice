4f6e196547037ec7c29d831a65a4d5d0363b3a35
fai7h01 <l.varsimashvil10@gmail.com>
2024-02-24 20:05:32 +0400

    Enum

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -1,5 +1,5 @@
 package com.cydeo;
 
 public enum Currency {
-    PENNY, NICKLE, DIME, QUARTER
+    PENNY, NICKLE, DIME, QUARTER // constant objects of Currency class
 }
--- m2-enum/src/main/java/com/cydeo/Main.java
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -7,6 +7,31 @@ public class Main {
 
         Currency c = Currency.DIME;
         System.out.println(c);
+        System.out.println(Currency.PENNY);
+
+        System.out.println("How to get all constant");
+        Currency[] currencies = Currency.values();
+
+        for (Currency currency : currencies){
+            System.out.println(currency);
+        }
+
+        System.out.println("How to use switch-case with enums");
+
+        switch (Currency.PENNY){
+            case PENNY:
+                System.out.println("It is 1 cent");
+                break;
+            case NICKLE:
+                System.out.println("It is 5 cent");
+                break;
+            case DIME:
+                System.out.println("It is 10 cent");
+                break;
+            case QUARTER:
+                System.out.println("It is 25 cent");
+                break;
+        }
 
     }
 }
