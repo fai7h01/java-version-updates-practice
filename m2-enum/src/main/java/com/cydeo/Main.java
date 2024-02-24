ea8a6ffd525c04897f48edb1ca49c704a664368f
fai7h01 <l.varsimashvil10@gmail.com>
2024-02-24 19:50:39 +0400

    Enum

---
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("How to access a constant");
+
+        Currency c = Currency.DIME;
+        System.out.println(c);
+
+    }
+}
