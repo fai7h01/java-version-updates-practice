facdf64ded61046459a2416b32de1b78736f3184
fai7h01 <l.varsimashvil10@gmail.com>
2024-02-26 01:29:22 +0400

    Enums review

--- m2-enum/src/main/java/com/cydeo/review/RoleEnum.java
+++ m2-enum/src/main/java/com/cydeo/review/RoleEnum.java
@@ -2,6 +2,12 @@ package com.cydeo.review;
 
 public enum RoleEnum {
 
-    ADMIN, MANAGER, EMPLOYEE
+    ADMIN("Admin"), MANAGER("Manager"), EMPLOYEE("Employee");
+
+    public String value;
+
+    RoleEnum(String value){
+        this.value = value;
+    }
 
 }
