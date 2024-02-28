6123970cd240da470c9b56ea6efff8cd99232779
fai7h01 <l.varsimashvil10@gmail.com>
2024-02-28 16:39:51 +0400

    Lambda - Lombok - new annotation - @Data

---
+++ m3-lambda/src/main/java/com/cydeo/tasks/User2.java
@@ -0,0 +1,18 @@
+package com.cydeo.tasks;
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@ToString
+@Data
+@AllArgsConstructor
+//@NoArgsConstructor
+public class User2 {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+}
