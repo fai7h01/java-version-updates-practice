10f315073d02f6fda04fd9fbf98b4a41ea6f5cd7
fai7h01 <l.varsimashvil10@gmail.com>
2024-02-28 16:31:04 +0400

    Lambda - functional interfaces - Predicate, Consumer. BiConsumer, Function, BiFunction, Supplier

--- m3-lambda/src/main/java/com/cydeo/FunctionalInterface.java
+++ m3-lambda/src/main/java/com/cydeo/FunctionalInterface.java
@@ -1,9 +1,6 @@
 package com.cydeo;
 
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class FunctionalInterface {
     public static void main(String[] args) {
@@ -30,6 +27,14 @@ public class FunctionalInterface {
         System.out.println("//************FUNCTION************//");
         Function<String, String> fun = s -> "Hello " + s;
         System.out.println(fun.apply("Lambda"));
+
+        System.out.println("//************BI FUNCTION************//");
+        BiFunction<Integer,Integer,Integer> func = (x1,x2) -> x1 * x2;
+        System.out.println(func.apply(10, 15));
+
+        System.out.println("//************SUPPLIER************//");
+        Supplier<Double> randomValue = () -> Math.random();
+        System.out.println(randomValue.get());
 
 
     }
