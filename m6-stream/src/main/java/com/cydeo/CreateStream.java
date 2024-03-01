4b5a277abf3b37fe538b7d6434c82cc477179780
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-01 21:04:36 +0400

    Stream - intermediate operations

--- m6-stream/src/main/java/com/cydeo/CreateStream.java
+++ m6-stream/src/main/java/com/cydeo/CreateStream.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Main {
+public class CreateStream {
     public static void main(String[] args) {
 
         //Creating stream from Array
@@ -22,6 +22,9 @@ public class Main {
         );
 
         Stream<Course> myCourseStream = myCourses.stream();
+
+        //Creating Stream values
+        Stream<Integer> stream = Stream.of(1,2,3,4,5);
 
 
     }
