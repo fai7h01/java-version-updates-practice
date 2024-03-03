30679fd7bcdc75682552d37d2d292e8259c10ed8
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-03 17:35:58 +0400

    Stream - intermediate operations flatMap()

--- m6-stream/src/main/java/com/cydeo/tasks/EmployeeData.java
+++ m6-stream/src/main/java/com/cydeo/tasks/EmployeeData.java
@@ -7,7 +7,7 @@ public class EmployeeData {
 
     public static Stream<Employee> readAll(){
         return Stream.of(
-                new Employee(100, "Mike", "mike@cydeo.com", Arrays.asList("2023434235","76343227875")),
+                new Employee(100, "Mike", "mike@cydeo.com", Arrays.asList("2023434235","7634322775")),
                 new Employee(100, "Luka", "luka@cydeo.com", Arrays.asList("5673434235","6534232365")),
                 new Employee(100, "Aaron", "aaron@cydeo.com", Arrays.asList("9873432235","3434245345"))
         );
--- m6-stream/src/main/java/com/cydeo/tasks/EmployeeTest.java
+++ m6-stream/src/main/java/com/cydeo/tasks/EmployeeTest.java
@@ -1,4 +1,20 @@
 package com.cydeo.tasks;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class EmployeeTest {
+    public static void main(String[] args) {
+
+        System.out.println("Print all emails --->");
+        EmployeeData.readAll()
+                .map(Employee::getEmpEmail)
+                .forEach(System.out::println);
+
+        System.out.println("Print all phone numbers");
+        EmployeeData.readAll()
+                .flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+
+    }
 }
