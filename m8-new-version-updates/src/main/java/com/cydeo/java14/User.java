be76349ef1d744d85a6cdc0e426a38a1aaf3071a
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-08 18:10:20 +0400

    java14 - new switch case syntax, record

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,14 @@
+package com.cydeo.java14;
+
+public record User(String name,String address) {
+
+    // each field is private final
+
+    //ALL ARG CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+
+}
---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
@@ -0,0 +1,12 @@
+package com.cydeo.java14;
+
+public class UserTest {
+    public static void main(String[] args) {
+
+        User user = new User("Luka", "TX");
+        System.out.println(user.name());
+        System.out.println(user.address());
+
+
+    }
+}
